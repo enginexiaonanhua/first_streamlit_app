be26415942d0be94ed3394c094b86b6d70003993
enginexiaonanhua <51066329+enginexiaonanhua@users.noreply.github.com>
2022-09-29 09:37:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,21 +29,16 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-if not fruit_choice:
-   streamlit.error("Please select a fruit to get information.")
-else:
-   
-                  
-   #streamlit.write('The user entered ', fruit_choice)
-
-
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-   #streamlit.text(fruityvice_response.json())# just writes the data to the screen
-
-   #take the json version of the response and normalize it
-   fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-   # output it the screen as table
-   streamlit.dataframe(fruityvice_normalized)
+   if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+   else:
+      #streamlit.write('The user entered ', fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      #streamlit.text(fruityvice_response.json())# just writes the data to the screen
+      #take the json version of the response and normalize it
+      fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+      # output it the screen as table
+      streamlit.dataframe(fruityvice_normalized)
 #do not run anything past here while we troubleshoot
 
 except  URLError as e:
